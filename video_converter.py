f2f0cba01c51653f46b8776ca2666f16ad6560b1
damianck <damian.wtr@gmail.com>
2017-05-10 19:47:53 +0200

    Update video_converter.py

--- video_converter.py
+++ video_converter.py
@@ -1,2 +1,10 @@
+import subprocess
+
 class VideoConverter():
-  pass
+        def create_video_from_image(self,image_path, video_output_path):
+                cmd="ffmpeg -loop 1 -i %s -t 2 -c:v libx264 -preset ultrafast -pix_fmt yuv420p %s"%(image_path,video_output_path)
+                subprocess.call(cmd, shell=True)
+
+
+
+
