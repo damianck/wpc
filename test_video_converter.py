84cdb5082094302e65f911d7866fcdf146fc7a0c
damianck <damian.wtr@gmail.com>
2017-05-10 18:51:16 +0200

    Create test_video_converter.py

---
+++ test_video_converter.py
@@ -0,0 +1,9 @@
+import unittest
+from media.video_converter import VideoConverter
+
+
+class TestVideoConverter(unittest.TestCase):
+        def test_transform_to_video(self):
+                converter=VideoConverter()
+
+
