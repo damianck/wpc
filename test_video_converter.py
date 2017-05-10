29a043afa8fa1e0c8f7c6a094c2257ac6b4add0c
damianck <damian.wtr@gmail.com>
2017-05-10 19:47:15 +0200

    Update test_video_converter.py

--- test_video_converter.py
+++ test_video_converter.py
@@ -1,3 +1,4 @@
+
 import unittest
 from media.video_converter import VideoConverter
 import os
@@ -5,18 +6,30 @@ import os
 
 class TestVideoConverter(unittest.TestCase):
         def setUp(self):
-                video_output_path= get_absolute_path('example.jpg')
+                video_output_path= self.get_absolute_path('example.jpg')
                 if os.path.isfile(video_output_path):
                         os.remove(video_output_path)
 
         def test_transform_to_video(self):
-                image_path= get_absolute_path('example.jpg')
-                video_output_path= get_absolute_path('example.mov')
+                image_path= self.get_absolute_path('example.jpg')
+                video_output_path= self.get_absolute_path('example.mov')
                 converter=VideoConverter()
                 converter.create_video_from_image(image_path, video_output_path)
                 assert os.path.exists(video_output_path) #isFile()
 
+        def test_create_transition_image_to_image(self):
+                image_path1 = self.get_absolute_path('example.jpg')
+                image_path2 = self.get_absolute_path('example2.jpg')
+                video_output_path = self.get_absolute_path('example.mov')
+
+                converter=VideoConverter()
+                converter.create_image_to_image_transition(image_path,image_path1, video_output_path)
+
+                assert os.path.exists(video_output_path)
+
         def get_absolute_path(self,path):
                 return os.path.join(os.path.dirname(__file__), path)
+
+
 
 
