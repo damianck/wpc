2c693f5c62c6b2acccd5cc11e67369c4c8348d1c
damianck <damian.wtr@gmail.com>
2017-05-10 19:37:29 +0200

    Update test_video_converter.py

--- test_video_converter.py
+++ test_video_converter.py
@@ -4,11 +4,19 @@ import os
 
 
 class TestVideoConverter(unittest.TestCase):
+        def setUp(self):
+                video_output_path= get_absolute_path('example.jpg')
+                if os.path.isfile(video_output_path):
+                        os.remove(video_output_path)
+
         def test_transform_to_video(self):
-                image_path= os.path.join(os.path.dirname(__file__), 'example.jpg')
-                video_output_path= os.path.join(os.path.dirname(__file__), 'example.mov')
+                image_path= get_absolute_path('example.jpg')
+                video_output_path= get_absolute_path('example.mov')
                 converter=VideoConverter()
                 converter.create_video_from_image(image_path, video_output_path)
                 assert os.path.exists(video_output_path) #isFile()
+
+        def get_absolute_path(self,path):
+                return os.path.join(os.path.dirname(__file__), path)
 
 
